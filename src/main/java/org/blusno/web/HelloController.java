bc674e66e7d483079bfdfb11232189585ecc100b
b1usn0 <blusno@126.com>
2016-09-21 13:52:05 +0800

    update

--- src/main/java/org/blusno/web/HelloController.java
+++ src/main/java/org/blusno/web/HelloController.java
@@ -3,9 +3,7 @@ package org.blusno.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/hello")
 public class HelloController {
 
     @RequestMapping("/index")
